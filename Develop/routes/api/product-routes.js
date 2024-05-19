78af323718a5f1a03fd435abd4d0f2fb00e436d3
Sam <sambolton.kw@gmail.com>
2024-05-19 01:44:04 -0400

    Think product route is finished

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -20,8 +20,7 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // find a single product by its `id` plus Category and Tag data
   try {
     const product = await Product.findByPk(req.params.id, {
       include: [{model: Category}, {model: Tag}],
@@ -40,6 +39,7 @@ router.get('/:id', async (req, res) => {
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
+  r
     {
       product_name: "Basketball",
       price: 200.00,
@@ -114,8 +114,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with that id!'});
+      return;
+    }
+
+    res.status(200).json(product)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
